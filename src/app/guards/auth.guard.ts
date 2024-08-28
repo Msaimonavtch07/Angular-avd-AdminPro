bb3b691b1f7cf44d2fd84d4a3b8685ffa207a486
Manuel Alvarado <saimon.03.architect@gmail.com>
2024-08-28 11:19:52 -0400

    Fien de la Seccion-19

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, GuardResult, MaybeAsync, Route, UrlSegment } from '@angular/router';
 
 import { tap } from 'rxjs/operators';
 import { UsuariosService } from '../services/usuarios.service';
@@ -7,10 +7,14 @@ import { UsuariosService } from '../services/usuarios.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor( private usuariosService: UsuariosService,
                private router: Router) {}
+
+  canLoad(route: Route, segments: UrlSegment[]): MaybeAsync<GuardResult> {
+    throw new Error('Method not implemented.');
+  };
 
   canActivate(
     next: ActivatedRouteSnapshot,
---
+++ src/app/pages/child-routes.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { AccountSettingComponent } from './account-setting/account-setting.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
+import { PerfilComponent } from './perfil/perfil.component';
+
+// CP. mantenimientos
+import { UsuariosComponent } from './mantenimientos/usuarios/usuarios.component';
+import { HospitalesComponent } from './mantenimientos/hospitales/hospitales.component';
+import { MedicosComponent } from './mantenimientos/medicos/medicos.component';
+import { MedicoComponent } from './mantenimientos/medicos/medico.component';
+import { BusquedasComponent } from './busquedas/busquedas.component';
+import { AdminGuard } from '../guards/admin.guard';
+
+
+const chaildRoutes: Routes = [
+
+  { path: '', component: DashboardComponent, data: { title: 'Dashboard' } },
+  { path: 'progress', component: ProgressComponent, data: { title: 'ProgreesBar' } },
+  { path: 'grafica1', component: Grafica1Component, data: { title: 'Gragicas' } },
+  { path: 'account-setting', component: AccountSettingComponent, data: { title: 'Temas' } },
+  { path: 'buscar/:termino', component: BusquedasComponent, data: { title: 'busquedas' } },
+  { path: 'promesas', component: PromesasComponent, data: { title: 'Promesas' } },
+  { path: 'rxjs', component: RxjsComponent, data: { title: 'RxJs' } },
+  { path: 'perfil', component: PerfilComponent, data: { title: 'Perfil de usuario' } },
+  /* { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, */
+
+  // CP. mantenimientos...
+
+  { path: 'hospitales', component: HospitalesComponent, data: { title: 'Mantenimientos de hospitales' } },
+  { path: 'medicos', component: MedicosComponent, data: { title: 'Mantenimientos de medicos' } },
+  { path: 'medico/:id', component: MedicoComponent, data: { title: 'Mantenimientos de medico' } },
+
+  // Ruta Admin
+  { path: 'usuarios', canActivate: [AdminGuard], component: UsuariosComponent, data: { title: 'Mantenimientos de usuarios' } },
+
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(chaildRoutes)],
+  exports: [RouterModule]
+})
+export class ChildRoutesModule { }
